50f042371efaf81192ba4f6081719ad77f3202e3
BarjinderBhumbla <bh.barjinder@gmail.com>
2025-07-22 17:04:37 +0530

    Changes in status types

--- routers/app_router.js
+++ routers/app_router.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
+
 const careerCandidateRouter = require("./user_router");
 const menusRouter = require("./menus_router");
 const facultiesRouter = require("./faculties_router");
-const programsRouter = require('./programs_router')
+const programsRouter = require('./programs_router');
+const statusTypesRouter = require("./admission/statustypes_router");
+
 router.use("/api/users", careerCandidateRouter);
 router.use("/api/menus", menusRouter);
-
-router.use("/api/faculties",facultiesRouter);
-router.use("/api/programs",programsRouter);
+router.use("/api/faculties", facultiesRouter);
+router.use("/api/programs", programsRouter);
+router.use("/api/statustypes", statusTypesRouter);
 
 router.get("/api/test", (req, res) => res.send("✅ Working"));
-
 
 module.exports = router;
