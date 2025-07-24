cddce3a8df666e4c4d6a3eda8f410a9f90440e34
AYUSH KUMAR <139211075+Ayushkumar302@users.noreply.github.com>
2025-07-24 10:45:53 +0530

    Update app_router.js

--- routers/app_router.js
+++ routers/app_router.js
@@ -6,7 +6,7 @@ const facultiesRouter = require("./academic/faculties_router");
 const programsRouter = require('./academic/programs_router');
 const classroomRouter = require("./academic/classroom_router");
 const batchesRouter = require("./academic/batches_router");
-
+const studentsRouter = require("./academic/student_routers");
 router.use("/api/users", careerCandidateRouter);
 router.use("/api/menus", menusRouter);
 
@@ -14,6 +14,7 @@ router.use("/api/faculties",facultiesRouter);
 router.use("/api/programs",programsRouter);
 router.use("/api/classrooms",classroomRouter);
 router.use("/api/batches",batchesRouter);
+router.use("/api/students", studentsRouter);
 
 router.get("/api/test", (req, res) => res.send("✅ Working"));
 
