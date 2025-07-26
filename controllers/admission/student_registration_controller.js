b448d9a374bdaa2f3d5de715929fd943252b0219
webdevsimr <webdevsimran@gmail.com>
2025-07-26 11:55:50 +0530

    working on student registartion

--- controllers/admission/student_registration_controller.js
+++ controllers/admission/student_registration_controller.js
@@ -1,5 +1,6 @@
 const logger = require("../../utils/logger");
 const { pgPool } = require("../../pg_constant");
+const { upload, uploadDir } = require("../../middlewares/multer");
 
 const getStudents = async (req, res) => {
   const page = parseInt(req.query.page) || 1;
