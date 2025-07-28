4326cb366d81b0a5b358144f0de41c84fba1dbb5
webdevsimr <webdevsimran@gmail.com>
2025-07-28 13:01:42 +0530

    working on sessions table

--- routers/academic/session_router.js
+++ routers/academic/session_router.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 const {
   getSessions,
+  createSession,
+  updateSession,
 } = require("../../controllers/academic/session_controller");
 
 router.get("/", getSessions);
+router.post("/create", createSession);
+router.put("/update/:id", updateSession);
 
 module.exports = router;
