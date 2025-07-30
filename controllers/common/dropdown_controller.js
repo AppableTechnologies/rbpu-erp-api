e2cf525c6102b974d65d65cd8b85a9d28343dcf9
BarjinderBhumbla <bh.barjinder@gmail.com>
2025-07-30 16:59:59 +0530

    added faculties

--- controllers/common/dropdown_controller.js
+++ controllers/common/dropdown_controller.js
@@ -29,13 +29,11 @@ const getSemesterDropdown = async (req, res) => {
 const getSectionDropdown = async (req, res) => {
   getDynamicDropdown(req, res, "sections");
 };
-<<<<<<< HEAD
 const getFacultyDropdown = async (req, res) => {
   getDynamicDropdown(req, res, "faculties");
-=======
+};
 const getStatusTypesDropdown = async (req, res) => {
   getDynamicDropdown(req, res, "status_types");
->>>>>>> f4453f6ef78a351547cd38031abfd6978f5e5141
 };
 
 module.exports = {
@@ -44,9 +42,6 @@ module.exports = {
   getSessionDropdown,
   getSemesterDropdown,
   getSectionDropdown,
-<<<<<<< HEAD
   getFacultyDropdown,
-=======
   getStatusTypesDropdown
->>>>>>> f4453f6ef78a351547cd38031abfd6978f5e5141
 };
--- routers/common/dropdown_router.js
+++ routers/common/dropdown_router.js
@@ -6,23 +6,18 @@ const {
   getSessionDropdown,
   getSemesterDropdown,
   getSectionDropdown,
-<<<<<<< HEAD
   getFacultyDropdown,
-=======
   getStatusTypesDropdown,
->>>>>>> f4453f6ef78a351547cd38031abfd6978f5e5141
 } = require("../../controllers/common/dropdown_controller");
-const { getStatusTypes } = require("../../controllers/admission/statustypes_controller");
+// const { getStatusTypes } = require("../../controllers/admission/statustypes_controller");
 
 dropdownRouter.get("/batches", getBatchesDropdown);
 dropdownRouter.get("/programs", getProgramsDropdown);
 dropdownRouter.get("/sessions", getSessionDropdown);
 dropdownRouter.get("/semesters", getSemesterDropdown);
 dropdownRouter.get("/sections", getSectionDropdown);
-<<<<<<< HEAD
 dropdownRouter.get("/faculties", getFacultyDropdown);
-=======
 dropdownRouter.get("/status_types", getStatusTypesDropdown);
->>>>>>> f4453f6ef78a351547cd38031abfd6978f5e5141
+
 
 module.exports = dropdownRouter;
