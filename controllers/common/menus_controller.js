cf46244d56a03f19aa2f24aab8c2dbff85be7916
webdevsimr <webdevsimran@gmail.com>
2025-07-31 10:16:12 +0530

    working on menus with orm

--- controllers/common/menus_controller.js
+++ controllers/common/menus_controller.js
@@ -1,4 +1,4 @@
-const {pgPool} = require("../pg_constant");
+const {pgPool} = require("../../pg_constant");
 
 
 
--- routers/app_router.js
+++ routers/app_router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const careerCandidateRouter = require("./user_router");
-const menusRouter = require("./menus_router");
+const menusRouter = require("./common/menus_router");
 const facultiesRouter = require("./academic/faculties_router");
 const programsRouter = require("./academic/programs_router");
 const classroomRouter = require("./academic/classroom_router");
