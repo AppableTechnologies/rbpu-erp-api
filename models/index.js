7dede7c658123d533f8d9ef56c8e9a5141ed9024
webdevsimr <webdevsimran@gmail.com>
2025-08-01 17:27:01 +0530

    working on updatePrograms controllerin program_controller

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const Classroom = require("./academic/Classroom.js");
+const Faculty = require("./academic/Faculty.js");
 const Program = require("./academic/Program.js");
 const ProgramSession = require("./academic/ProgramSession.js");
 
@@ -34,11 +35,15 @@ Program.hasMany(ProgramSession, { foreignKey: "program_id" });
 ProgramSession.belongsTo(Session, { foreignKey: "session_id" });
 ProgramSession.belongsTo(Program, { foreignKey: "program_id" });
 
+Program.belongsTo(Faculty, { foreignKey: "faculty_id", as: "faculty" });
+// Faculty.hasMany(Program, { foreignKey: "faculty_id", as: "programs" });
+
 module.exports = {
   Menu,
   Submenu,
   Classroom,
   Program,
+  Faculty,
   ProgramSession,
   Session,
 };
