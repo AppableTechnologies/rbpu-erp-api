2b3713bd9d694b79a9196573a81739b654103867
Ayushkumar302 <ayushxkumar2002@gmail.com>
2025-08-21 16:13:20 +0530

    fixed sidebar menu

--- controllers/common/menus_controller.js
+++ controllers/common/menus_controller.js
@@ -117,3 +117,79 @@ module.exports.getMenus = async function getMenus(req, res) {
     return res.status(500).send("Server Error");
   }
 };
+
+// **************************  
+// module.exports.getMenus = async function getMenus(req, res) {
+//   try {
+//     const rid = req.params.role_id_fk;
+//     console.log("Role ID of logged-in user:", rid);
+
+//     const menus = await Menu.findAll({
+//       where: {
+//         active: true,
+//         role_id_fk: rid,
+//       },
+//       order: [["menu_order", "ASC"]],
+//       include: [
+//         {
+//           model: Submenu,
+//           as: "submenus",
+//           where: {
+//             is_active: true,
+//           },
+//           required: false,
+//           order: [["submenu_order", "ASC"]],
+//           attributes: [
+//             "submenu_id_pk",
+//             "menu_id_fk",
+//             "title",
+//             "submenu_icon",
+//             "path",
+//             "type",
+//           ],
+//         },
+//       ],
+//       attributes: [
+//         "id",
+//         "title",
+//         "icon",
+//         "path",
+//         "type",
+//         "slicedpath",
+//         "active",
+//       ],
+//     });
+
+//     // Transform to your desired format
+//     const transformedMenus = menus.map((menu) => {
+//       const children = (menu.submenus || []).map((submenu) => ({
+//         path: submenu.path,
+//         title: submenu.title,
+//         type: submenu.type,
+//       }));
+
+//       return {
+//         id: menu.id,
+//         title: menu.title,
+//         icon: menu.icon,
+//         type: menu.type,
+//         slicedpath: menu.slicedpath,
+//         active: menu.active,
+//         ...(children.length > 0 && { children }),
+//       };
+//     });
+
+//     // Return just the transformed menus without the category wrapper
+//     return res.status(200).json(transformedMenus);
+//   } catch (err) {
+//     console.error("Menu Fetch Error:", err.message);
+//     return res.status(500).send("Server Error");
+//   }
+// };
+
+
+
+
+
+
+
